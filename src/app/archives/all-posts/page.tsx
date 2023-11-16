74a4fa77bd7cf370d094a49a046a067a87c636f6
Mitchell Opatowsky <opatowsky77@gmail.com>
2023-11-16 14:26:28 -0600

    made great progress on the landing page, updating recent cards with contentful api data and slicing that sorted map function

--- src/app/archives/all-posts/page.tsx
+++ src/app/archives/all-posts/page.tsx
@@ -1,44 +1,59 @@
-import { ContentfulApi } from "@/lib/contentfulApi";
-import { Metadata } from 'next';
+import contentfulApiInstance, { ContentfulApi } from "@/lib/contentfulApi";
+import { Metadata } from "next";
 import { detailedServerLogger } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
-
-const contentful = new ContentfulApi();
-export async function generateStaticParams() {
-    const paths = await contentful.getPaths();
-    console.log('paths!', paths);
-    return paths;
-}
+import { FetchAllBlogEntriesReturnType } from "@/types/types";
 
 export const dynamicParams = false;
 
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export default async function Page({ params }: {params : {slug: string} }) {
+async function getAllPosts() {
+  const blogPosts: FetchAllBlogEntriesReturnType =
+    await contentfulApiInstance.fetchAllBlogEntries();
 
-    const { slug } = params;
+  return blogPosts;
+}
 
-    const postData = await contentful.fetchAllBlogEntries()
+export default async function Page({ params }: { params: { slug: string } }) {
+  const blogPosts = await getAllPosts();
 
-    
-    return (
-        <>
-            <div className="p-6 bg-gray-100">
-                <h1 className="text-4xl font-bold mb-8 text-gray-800">All Posts</h1>
-                <h2 className="text-3xl my-2">Total Posts: {postData?.total}</h2>
-                {postData && postData.blogPosts.map((post) => {
-                    return (
-                        <div key={post.id} className="mb-10 p-4 rounded shadow-md bg-white hover:shadow-lg transition-shadow duration-300">
-                            <h2 className="text-3xl font-semibold text-gray-900 mb-2">{post.title}</h2>
-                            <p className="text-lg text-gray-700 mb-1">Subtitle: {post.subtitle}</p>
-                            <p className="text-sm text-gray-500 mb-1">Published: {post.date}</p>
-                            <p className="text-sm text-gray-500 mb-1">By: {post.author?.fields.name as string}</p>
-                            <p className="text-sm text-gray-500">Tag: {post.tag.title}</p>
-                        </div>
-                    )
-                })}
-            </div>
-        </>
-    );
-    
+  if (!blogPosts) {
+    return null;
   }
 
+  return (
+    <>
+      <div className="p-6 bg-gray-100">
+        <h1 className="text-4xl font-bold mb-8 text-gray-800">All Posts</h1>
+        <h2 className="text-3xl my-2">Total Posts: {blogPosts?.total}</h2>
+        {blogPosts &&
+          blogPosts?.blogPosts.map((post) => {
+            return (
+              <div
+                key={post.id}
+                className="mb-10 p-4 rounded shadow-md bg-white hover:shadow-lg transition-shadow duration-300"
+              >
+                <h2 className="text-3xl font-semibold text-gray-900 mb-2">
+                  {post.title}
+                </h2>
+                <p className="text-lg text-gray-700 mb-1">
+                  Subtitle: {post.subtitle}
+                </p>
+                <p className="text-sm text-gray-500 mb-1">
+                  Published: {post.date}
+                </p>
+                <p className="text-sm text-gray-500 mb-1">
+                  By: {post.author?.fields.name as string}
+                </p>
+                {/* <p className="text-sm text-gray-500">Tag: {tagSlug}</p> */}
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
--- src/app/home/recent-articles/index.tsx
+++ src/app/home/recent-articles/index.tsx
@@ -1,11 +1,76 @@
-import { data } from "./sample-data";
-import { RecentCard } from "./recent-card";
+import contentfulApiInstance, { ContentfulApi } from "@/lib/contentfulApi";
+// import { data } from "./sample-data";
+import { RecentCard, RecentCardProps } from "./recent-card";
 import { Button, ButtonVariants } from "@/components/ui/button";
 import { Paragraph } from "@/components/elements";
+import {
+  FetchAllBlogEntriesBlogPostType,
+  FetchAllBlogEntriesReturnType,
+} from "@/types/types";
+
+// ADDED FUNCTIONS
+
+const contentful = new ContentfulApi();
+export async function generateStaticParams() {
+  const paths = await contentful.getTagPaths();
+  console.log("paths!", paths);
+  return paths;
+}
+
+export const dynamicParams = false;
+
+async function getAllPosts() {
+  const blogPosts: FetchAllBlogEntriesReturnType =
+    await contentfulApiInstance.fetchAllBlogEntries();
+
+  return blogPosts;
+}
+
+function convertToRecentCardProps(
+  blogPost: {
+    coverImage: { imageUrl: string };
+    title: any;
+    subtitle: any;
+    tag: { slug: any; title: string };
+    slug: any;
+  },
+  delay: number
+): RecentCardProps {
+  return {
+    imageUrl: blogPost.coverImage ? blogPost.coverImage.imageUrl : "",
+    title: blogPost.title,
+    subtitle: blogPost.subtitle,
+    link: `/blog/${blogPost.tag.slug}/${blogPost.slug}`,
+    tag: blogPost.tag.title as string,
+    delay: delay,
+  };
+}
+
+// FOR RECENT ARTICLES
 
 const bgColorClasses = ["bg-color-0", "bg-color-1", "bg-color-2", "bg-color-3"];
 
-export default function RecentArticles() {
+export default async function RecentArticles() {
+  // const blogPosts = await getAllPosts();
+
+  // if (!blogPosts) {
+  //   return null;
+  // }
+
+  // const sortedPosts = blogPosts.sort((a, b) => new Date(b.publishedDate) - new Date(a.publishedDate));
+
+  const blogPostsData = await getAllPosts();
+
+  if (!blogPostsData || !blogPostsData.blogPosts) {
+    return null;
+  }
+
+  const sortedPosts = blogPostsData.blogPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  const recentPosts = sortedPosts.slice(0, 5);
+
   return (
     // <section className="container my-0 px-0 mx-0">
     // <section className="my-0 px-0 mx-0">
@@ -19,15 +84,21 @@ export default function RecentArticles() {
       <div className="2xl:flex 2xl:mx-auto">
         <div className="hidden 2xl:block 2xl:w-1/12" />
         <div className="flex flex-wrap justify-start md:justify-center gap-0 px-0 mx-0 2xl:mx-12 2xl:w-10/12">
-          {data.map((item, index) => (
-            <RecentCard
-              key={item.title}
-              {...item}
-              customBgClass={bgColorClasses[index % bgColorClasses.length]}
-              // isTopCard={index === 0}
-              // customWidth={index === 0 ? "md:w-10/12" : "md:w-5/12"}
-            />
-          ))}
+          {recentPosts &&
+            recentPosts?.map((post, index) => {
+              if (!post) return null;
+              const recentBlogPost = convertToRecentCardProps(post, 0);
+              return (
+                <div key={post.id} className="mb-6">
+                  <RecentCard
+                    {...recentBlogPost}
+                    customBgClass={
+                      bgColorClasses[index % bgColorClasses.length]
+                    }
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="hidden 2xl:block 2xl:w-1/12" />
       </div>
