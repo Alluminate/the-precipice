3e1a429295028683d3d8e64def4e0b2076e635f0
Femi Orokunle <femiorokunle@gmail.com>
2023-12-21 18:40:48 -0800

    Fixing route error for /contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,12 @@
 // import { Hero } from "./components";
 // import { FAQS } from "../services/components";
 
-// export default function ContactPage() {
-//   return (
-//     <section className='space-y-20'>
-//       <Hero />
-//       <FAQS />
-//     </section>
-//   )
-//  }
+export default function ContactPage() {
+  return (
+    <></>
+    // <section className='space-y-20'>
+    //   <Hero />
+    //   <FAQS />
+    // </section>
+  )
+ }
